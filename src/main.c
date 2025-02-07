570d118504620b87b01c9ca99a39603a953272c6
Liam Tanner <liamtanner@uvic.ca>
2025-02-06 17:44:26 -0800

    initial files

---
+++ src/main.c
@@ -0,0 +1,180 @@
+
+/* Standard includes. */
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f4_discovery.h"
+/* Kernel includes. */
+#include "stm32f4xx.h"
+#include "../FreeRTOS_Source/include/FreeRTOS.h"
+#include "../FreeRTOS_Source/include/queue.h"
+#include "../FreeRTOS_Source/include/semphr.h"
+#include "../FreeRTOS_Source/include/task.h"
+#include "../FreeRTOS_Source/include/timers.h"
+
+
+
+/*-----------------------------------------------------------*/
+
+
+/*
+ * TODO: Implement this function for any hardware specific clock configuration
+ * that was not already performed before main() was called.
+ */
+static void prvSetupHardware( void );
+
+/*
+ * The queue send and receive tasks as described in the comments at the top of
+ * this file.
+ */
+static void Manager_Task( void *pvParameters );
+
+xQueueHandle xQueue_handle = 0;
+
+void led_init( void );
+void led_data( unsigned char data );
+void adc_init( void );
+void status_toggle( void );
+uint16_t adc_convert( void );
+
+void Delay( void );
+
+unsigned char digits[10] = { 0x3f, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
+
+
+/*-----------------------------------------------------------*/
+
+int main(void)
+{
+
+	adc_init();
+	led_init();
+
+	/* Configure the system ready to run the demo.  The clock configuration
+	can be done here if it was not done before main() was called. */
+	prvSetupHardware();
+
+
+	xTaskCreate( Manager_Task, "Manager", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+	printf("Hi there\n");
+	/* Start the tasks and timer running. */
+	vTaskStartScheduler();
+
+	return 0;
+}
+
+
+/*-----------------------------------------------------------*/
+
+static void Manager_Task( void *pvParameters )
+{
+//
+// Display mode range 1 to 3
+//
+	uint16_t mode = 1;
+
+	uint16_t counter = 0;
+	uint16_t delay = 0;
+
+	uint16_t adc_data = 0;
+
+	while(1)
+	{
+		adc_data = adc_convert() / 409;
+		printf("pot value %d\n", adc_data);
+		if ( adc_data >= 10 )
+		{
+			adc_data = 9;
+		}
+
+
+		if ( mode == 1 )
+		{
+			status_toggle();
+			delay = 250;
+			led_data( digits[counter] );
+		}
+		else if ( mode == 2 )
+		{
+			delay = 250;
+			led_data( digits[adc_data] );
+		}
+		else
+		{
+			delay = (100*adc_data) + 100;
+			led_data( digits[counter] );
+		}
+
+		counter = counter + 1;
+		if ( counter >= 10 )
+		{
+			counter = 0;
+		}
+
+
+		vTaskDelay( delay );
+	}
+}
+
+
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+	/* The malloc failed hook is enabled by setting
+	configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
+
+	Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software 
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( xTaskHandle pxTask, signed char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configconfigCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected.  pxCurrentTCB can be
+	inspected in the debugger if the task name passed into this function is
+	corrupt. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+volatile size_t xFreeStackSpace;
+
+	/* The idle task hook is enabled by setting configUSE_IDLE_HOOK to 1 in
+	FreeRTOSConfig.h.
+
+	This function is called on each cycle of the idle task.  In this case it
+	does nothing useful, other than report the amount of FreeRTOS heap that
+	remains unallocated. */
+	xFreeStackSpace = xPortGetFreeHeapSize();
+
+	if( xFreeStackSpace > 100 )
+	{
+		/* By now, the kernel has allocated everything it is going to, so
+		if there is a lot of heap remaining unallocated then
+		the value of configTOTAL_HEAP_SIZE in FreeRTOSConfig.h can be
+		reduced accordingly. */
+	}
+}
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+	/* Ensure all priority bits are assigned as preemption priority bits.
+	http://www.freertos.org/RTOS-Cortex-M3-M4.html */
+	NVIC_SetPriorityGrouping( 0 );
+
+	/* TODO: Setup the clocks, etc. here, if they were not configured before
+	main() was called. */
+}
+
